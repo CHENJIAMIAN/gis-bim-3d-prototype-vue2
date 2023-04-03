2001afd116741d179d433a069b8ce1a32aa87791
chenjiamian <571574085@qq.com>
2023-04-03 18:39:39 +0800

    add tailwind latest

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <div id="app">
+    <h1 class="text-3xl font-bold underline bg-red-100">
+    Hello world!
+    </h1>
     <header>
       <img
         alt="Vue logo"
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createPinia, PiniaVuePlugin } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "./main.css";
+
+
 import "./assets/main.css";
 
 Vue.use(PiniaVuePlugin);
